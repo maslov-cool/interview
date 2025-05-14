0f784d34a1083551c71169cdabdee926b1ac5e73
alex <106116143+maslov-cool@users.noreply.github.com>
2025-05-15 01:43:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+import asyncio
+
+
+async def main(name, time_prepare_1, time_protection_1, time_prepare_2, time_protection_2):
+    print(f'{name} started the 1 task.')
+    await asyncio.sleep(time_prepare_1 / 100)
+
+    print(f'{name} moved on to the defense of the 1 task.')
+    await asyncio.sleep(time_protection_1 / 100)
+
+    print(f'{name} completed the 1 task.')
+    print(f'{name} is resting.')
+    await asyncio.sleep(5 / 100)
+
+    print(f'{name} started the 2 task.')
+    await asyncio.sleep(time_prepare_2 / 100)
+
+    print(f'{name} moved on to the defense of the 2 task.')
+    await asyncio.sleep(time_protection_2 / 100)
+
+    print(f'{name} completed the 2 task.')
+
+
+async def interviews(*args):
+    tasks = [
+        asyncio.create_task(main(*i))
+        for i in args
+    ]
+    await asyncio.gather(*tasks)
+
+
+data = [('Ivan', 5, 2, 7, 2), ('John', 3, 4, 5, 1), ('Sophia', 4, 2, 5, 1)]
+t0 = time.time()
+asyncio.run(interviews(*data))
+print(time.time() - t0)
